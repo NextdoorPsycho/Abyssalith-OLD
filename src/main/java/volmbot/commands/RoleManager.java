93885603cf1d7c9aff025d71cd10520c60e7faef
Brian Fopiano <mypsn1550@gmail.com>
2021-08-15 04:52:12 -0700

    ddd

---
+++ src/main/java/volmbot/commands/RoleManager.java
@@ -0,0 +1,29 @@
+package volmbot.commands;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import volmbot.util.VolmitCommand;
+import volmbot.util.VolmitEmbed;
+import java.util.List;
+
+public class Ping extends VolmitCommand {
+    // Constructor
+    public Ping() {
+        super(
+                "ping",
+                new String[]{"ping", "p"},
+                new String[]{}, // Always permitted if empty. User must have at least one if specified.
+                "Useful for Pings!",
+                false,
+                null
+        );
+    }
+    // Handle
+    @Override
+    public void handle(List<String> args, GuildMessageReceivedEvent e) {
+        VolmitEmbed embed = new VolmitEmbed(" PONG!", e.getMessage());
+        //Commands
+        //embed.addField("Name Here", "" + "Value here", false);
+        embed.send(e.getMessage(), true, 1000);
+
+    }
+}
---
+++ src/main/java/volmbot/listeners/ReactionListener.java
@@ -0,0 +1,2 @@
+package volmbot.listeners;public class ReactionListener {
+}
